14e8b75f15945ae4c31fb575060d04d0f9058bae
apple <apple@apples-imac-6.attlocal.net>
2021-04-03 09:36:32 -0400

    update

---
+++ src/javainterviewcodequestions/InterviewQuestions34.java
@@ -0,0 +1,18 @@
+package javainterviewcodequestions;
+
+public class InterviewQuestions34 {
+	
+	/*
+ 		Type a program to print the integers from 0 to 100 without using any integer
+    */
+
+public static void main(String[] args) {
+	
+	for(char c=' '; c<'e'; c++) {
+		
+		System.out.println((int)c);
+		
+	}
+
+}
+}
