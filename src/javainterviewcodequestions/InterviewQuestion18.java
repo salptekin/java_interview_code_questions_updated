caa305ae39e877385e8ff4cf1220f0237838983a
apple <apple@apples-imac-6.attlocal.net>
2021-11-20 16:40:31 -0500

    Committed

--- src/javainterviewcodequestions/InterviewQuestion18.java
+++ src/javainterviewcodequestions/InterviewQuestion18.java
@@ -1,33 +1,34 @@
 package javainterviewcodequestions;
 
-public class InterviewQuestion18 {
-	
 	/*
-	  	Ask user to enter the number of lines of a pyramide.
-	  	Type a program to create the pyramide.
-	  	For example if the number of lines is 5, the pyramide will be like;         * 
+	 Ask user to enter the number of lines of a pyramide.
+	  Type a program to create the pyramide.
+	  For example if the number of lines is 5, the pyramide will be like;           * 
 																			      * * * 
 																			    * * * * * 
 																			  * * * * * * * 
 																			* * * * * * * * *
-	*/
+	 */
+
+public class InterviewQuestion18 {
 
 	public static void main(String[] args) {
+
+		int numOfRows = 5; 
 		
-	    int rows = 5, k = 0;
-
-	    for (int i = 1; i <= rows; i++, k = 0) {
-	    	
-	      for (int space = 1; space <= rows - i; space++) {
-	        System.out.print("  ");
-	      }
-
-	      while (k != 2 * i - 1) {
-	        System.out.print("* ");
-	        k++;
-	      }
-
-	      System.out.println();
-	    }
+		for(int i=1; i<=numOfRows; i++) {
+			
+			for(int space = 1; space<=numOfRows-i; space++) {
+				System.out.print("  ");
+			}
+			
+			for(int k=0; k<2*i-1; k++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+			
+		}
 	}
+
 }
--- src/javainterviewcodequestions/InterviewQuestions33.java
+++ src/javainterviewcodequestions/InterviewQuestions33.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
